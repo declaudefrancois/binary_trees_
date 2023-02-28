157aefda46363fad2c26269d514c7be74c0d75e0
NKOUKA Guy François de Claude <declaudefrancois@gmail.com>
2023-02-28 23:13:42 +0000

    feat: function that goes through a binary tree using in-order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_inorder - Goes through a binary tree
+ * using in-order traversal.
+ *
+ * @tree: A pointer to the root of the tree.
+ * @func: A function pointer being called for each node
+ * during travesal.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
