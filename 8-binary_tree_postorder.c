9ecd9ab940f606d67724523913dab8dd5db58b00
KINI Biè Honoré <104803215+honokini@users.noreply.github.com>
2023-03-02 10:31:59 +0000

    Commit tasks 8 to 13

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,16 +2,15 @@
 
 /**
  * binary_tree_postorder - goes through a binary tree using post-order traverse
- * @tree: tree to traverse
- * @func: pointer to a function to call for each node
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-if (tree == NULL || func == NULL)
-return;
+	if (!tree || !func)
+		return;
 
-binary_tree_postorder(tree->left, func);
-binary_tree_postorder(tree->right, func);
-func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
