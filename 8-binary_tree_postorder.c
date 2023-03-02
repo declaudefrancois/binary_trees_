6bf72d8a2ac9e0a9e3a9a0afa740ebab7b92c510
KINI Biè Honoré <104803215+honokini@users.noreply.github.com>
2023-03-02 09:29:37 +0000

    bobobobo

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,12 +5,13 @@
  * @tree: tree to traverse
  * @func: pointer to a function to call for each node
  */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
+    if (tree == NULL || func == NULL)
+        return;
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+    binary_tree_postorder(tree->left, func);
+    binary_tree_postorder(tree->right, func);
+    func(tree->n);
 }
